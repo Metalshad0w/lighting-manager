cc67c67a1bc7bd6ec2707ee528966ecbe3b46fba
Metalshad0w <wralucas@hotmail.com>
2020-11-05 18:35:57 -0300

    Minor improvements

--- lightManager.py
+++ lightManager.py
@@ -11,22 +11,19 @@ GPIO.setwarnings(False)
 dutyCicle = 50
 
 #define actuators GPIOs
-ch1 = 26 #GPIO26
-ch2 = 19 #GPIO19
-ch3 = 13 #GPIO13
-ch4 = 6  #GPIO06
+ch = [26, 19, 13, 6] #GPIO26, GPIO19, GPIO13 and GPIO06
 
 #define channel pins as output
-GPIO.setup(ch1, GPIO.OUT)
-GPIO.setup(ch2, GPIO.OUT)
-GPIO.setup(ch3, GPIO.OUT)
-GPIO.setup(ch4, GPIO.OUT)
+GPIO.setup(ch[0], GPIO.OUT)
+GPIO.setup(ch[1], GPIO.OUT)
+GPIO.setup(ch[2], GPIO.OUT)
+GPIO.setup(ch[3], GPIO.OUT)
 
 #turn all channels OFF 
-GPIO.output(ch1, GPIO.LOW)
-GPIO.output(ch2, GPIO.LOW)
-GPIO.output(ch3, GPIO.LOW)
-GPIO.output(ch4, GPIO.LOW)
+GPIO.output(ch[0], GPIO.LOW)
+GPIO.output(ch[1], GPIO.LOW)
+GPIO.output(ch[2], GPIO.LOW)
+GPIO.output(ch[3], GPIO.LOW)
 
 #get the time value and format
 now = datetime.datetime.now()
@@ -36,19 +33,9 @@ timeString = now.strftime("%H")
 with open('lightConfig.json') as json_file:
     data = json.load(json_file)
     for conf in data['settings']:
-        if timeString == conf['time']: 
-            print('Time: ' + conf['time'])
-            print('CH1: ' + conf['ch1'])
-            print('CH2: ' + conf['ch2'])
-            print('CH3: ' + conf['ch3'])
-            print('CH4: ' + conf['ch4'])
-            print('')
-            
-            signCH1=GPIO.PWM(ch1,dutyCicle)
-            signCH1.start(int(conf['ch1']))
-            signCH1=GPIO.PWM(ch2,dutyCicle)
-            signCH1.start(int(conf['ch2']))
-            signCH1=GPIO.PWM(ch3,dutyCicle)
-            signCH1.start(int(conf['ch3']))
-            signCH1=GPIO.PWM(ch4,dutyCicle)
-            signCH1.start(int(conf['ch4']))
+        if timeString == conf['time']:        
+            for x in range(0, len(ch)):
+                actuator="ch%d" % (x)
+                print("CH%d: " % (x) + conf[actuator])
+                signCH=GPIO.PWM(ch[x],dutyCicle)
+                signCH.start(int(conf[actuator]))
--- powerPlugManager.py
+++ powerPlugManager.py
@@ -8,47 +8,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 #define actuators GPIOs
-pw1 = 21 #GPIO21
-pw2 = 20 #GPIO20
-pw3 = 16 #GPIO16
-pw4 = 12 #GPIO12
-pw5 = 7  #GPIO07
-pw6 = 8  #GPIO08
-pw7 = 25 #GPIO25
-pw8 = 24 #GPIO24
-pw9 = 23 #GPIO23
-pw10 = 18 #GPIO18
-pw11 = 15 #GPIO15
-pw12 = 14 #GPIO14
+pw = [21, 20, 16, 12, 7, 8, 25, 24, 23, 18, 15, 14] #GPIO21, etc
 
-
-#define power plug pins as output
-GPIO.setup(pw1, GPIO.OUT)
-GPIO.setup(pw2, GPIO.OUT)
-GPIO.setup(pw3, GPIO.OUT)
-GPIO.setup(pw4, GPIO.OUT)
-GPIO.setup(pw5, GPIO.OUT)
-GPIO.setup(pw6, GPIO.OUT)
-GPIO.setup(pw7, GPIO.OUT)
-GPIO.setup(pw8, GPIO.OUT)
-GPIO.setup(pw9, GPIO.OUT)
-GPIO.setup(pw10, GPIO.OUT)
-GPIO.setup(pw11, GPIO.OUT)
-GPIO.setup(pw12, GPIO.OUT)
-
-#turn all channels OFF 
-GPIO.output(pw1, GPIO.LOW)
-GPIO.output(pw2, GPIO.LOW)
-GPIO.output(pw3, GPIO.LOW)
-GPIO.output(pw4, GPIO.LOW)
-GPIO.output(pw5, GPIO.LOW)
-GPIO.output(pw6, GPIO.LOW)
-GPIO.output(pw7, GPIO.LOW)
-GPIO.output(pw8, GPIO.LOW)
-GPIO.output(pw9, GPIO.LOW)
-GPIO.output(pw10, GPIO.LOW)
-GPIO.output(pw11, GPIO.LOW)
-GPIO.output(pw12, GPIO.LOW)
+#define power plug pins as output and turn all channels OFF 
+for powerPlug in pw:
+    GPIO.setup(pw, GPIO.OUT)
+    GPIO.output(pw, GPIO.LOW)
 
 #get the time value and format
 now = datetime.datetime.now()
@@ -58,19 +23,9 @@ with open('powerPlugConfig.json') as json_file:
     data = json.load(json_file)
     for conf in data['settings']:
         if timeString == conf['time']: 
-            print('Time: ' + conf['time'])
-            print('PW1: ' + conf['pw1'])
-            print('PW2: ' + conf['pw2'])
-            print('PW3: ' + conf['pw3'])
-            print('PW4: ' + conf['pw4'])
-            print('PW5: ' + conf['pw5'])
-            print('PW6: ' + conf['pw6'])
-            print('PW7: ' + conf['pw7'])
-            print('PW8: ' + conf['pw8'])
-            print('PW9: ' + conf['pw9'])
-            print('PW10: ' + conf['pw10'])
-            print('PW11: ' + conf['pw11'])
-            print('PW12: ' + conf['pw12'])
-            print('')
-            
-            GPIO.output(actuator, GPIO.HIGH)
+            for x in range(0, len(pw)):
+                actuator="pw%d" % (x)
+                print(conf['time'])
+                status = GPIO.HIGH
+                print(status)
+                GPIO.output(pw[x], status)
